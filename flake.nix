0243484c7f8bfc7312b562f68ed2493c5136b2a0
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:26 -0300

    chore: re index exporttosqlite3

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."exporttosqlite3-main".dir   = "main";
+  inputs."exporttosqlite3-main".owner = "nim-nix-pkgs";
+  inputs."exporttosqlite3-main".ref   = "master";
+  inputs."exporttosqlite3-main".repo  = "exporttosqlite3";
+  inputs."exporttosqlite3-main".type  = "github";
+  inputs."exporttosqlite3-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."exporttosqlite3-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
